f7bc6b8015bb5cf39dc620b5d5ed9e0b89104d4e
ilyHamovd <76834197+ilyHamovd@users.noreply.github.com>
2021-05-28 15:01:11 +0400

    Update hehehehe.js

--- hehehehe.js
+++ hehehehe.js
@@ -9,12 +9,21 @@ client.on('ready', () => {
   client.user.setActivity(`$help`, { type: "PLAYING" });
 });
 
+
+
+
+
 client.on('message', msg => {
   if (msg.content === 'hello') {
     msg.reply('Hi 👋');
   }
 });
 
+client.on('message', message => {
+  if (message.content === prefix + 'ping') {  
+    message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+  }
+});
 
 client.on("message", msg => {
   if(msg.content.startsWith (prefix + "help")) {
@@ -42,9 +51,26 @@ client.on('message', message => {
         .setImage(`${getvalueof.avatarURL}`);
         message.channel.sendEmbed(avatar);
     }
-}); 
-   
-          
-          
-          
+});
+
+client.on('message', function (msg) {
+  if (msg.content.startsWith(prefix + 'server')) {
+    let embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setThumbnail(msg.guild.iconURL)
+      .setTitle(`Server Statistics.✨`)
+      .addField(':globe_with_meridians:~**Server Region**', `[** __${msg.guild.region}__ **]`, true)
+      .addField(':medal:~**Roles**', `[** __${msg.guild.roles.size}__ **]`, true)
+      .addField(':red_circle:~**Members**', `[** __${msg.guild.memberCount}__ **]`, true)
+      .addField(':blue_circle:~**Online Members**', `[** __${msg.guild.members.filter(m => m.presence.status == 'online').size}__ **]`, true)
+      .addField(':pencil:~**Text Rooms**', `[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`, true)
+      .addField(':microphone:~**Voice Rooms**', `[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`, true)
+      .addField(':crown:~**Server Owner**', `**${msg.guild.owner}**`, true)
+      .addField(':id:~**Server ID**', `**${msg.guild.id}**`, true)
+      .addField(':date:~**Server Created on**', msg.guild.createdAt.toLocaleString())
+    msg.channel.send({ embed: embed });
+  }
+});
+
+
           client.login(process.env.BOT_TOKEN);
